99a095d11e503f40903ed074ec760a25d482536e
robbiemie <yang657850144@163.com>
2021-10-09 17:11:23 +0800

    feat: allSettled

--- src/编程题/allSettled.js
+++ src/编程题/allSettled.js
@@ -1,20 +1,26 @@
 // ; 能不能实现 Primise.allSettled，解决这个文档，无论是正常请求还是异常请求都能被 resolve 返回；
 
 Promise.prototype.allSettled = function (...args) {
-    let length = args.length
-    while(length) {
-        for(let i =0;i<args.length;i++) {
-            args[i].then(res => {
-                length--
-                resolve(res)
-            }).catch(e=> {
-                length--
-                resolve({
-                    code: -1,
-                    error: e?.stack
+    let arr = []
+    return new Promise(resolve => {
+        args.forEach((promise,index) => {
+            promise.then(res => {
+                arr[index] = ({
+                    code: 0,
+                    data: res
                 })
+            }).catch(error=> {
+                arr[index] = ({
+                    code: -1,
+                    error
+                })
+            }).finally(_ => {
+                if(arr.length === args.length) {
+                    resolve(arr)
+                }
             })
-        }
-    }
-    return Promise.resolve()
+        })
+    })
 }
+
+
