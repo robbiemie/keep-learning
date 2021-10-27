5044c8618fdf3ed26943de39db0cb40180a7e4ff
robbiemie <yang657850144@163.com>
2021-10-27 16:57:24 +0800

    feat: 括号生成

---
+++ src/动态规划/括号生成.js
@@ -0,0 +1,33 @@
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * 有效括号组合需满足：左括号必须以正确的顺序闭合。
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = []
+    if(n === 0) return result
+    const generate = (lCount, rCount, stack) => {
+
+        if(lCount < rCount) return
+
+        if(lCount === n && rCount === n) {
+            result.push(stack.join())
+            return
+        }
+    
+        if(lCount < n) {
+            generate(lCount + 1, rCount, stack.concat('('))
+        }
+        if(rCount < lCount) {
+            generate(lCount, rCount + 1, stack.concat(')'))
+        }
+    }
+
+    generate(lCount, rCount, [])
+    return result
+};
