a02346e1633b366e4f77398119f3b8676ad24a1e
robbiemie <yang657850144@163.com>
2021-11-02 17:46:26 +0800

    feat: 剪绳子

--- src/动态规划/剪绳子.js
+++ src/动态规划/剪绳子.js
@@ -1,15 +1,14 @@
 /**
  * 剪绳子
- * f(4) = f(2) * f(2) = 4
- * f(5) = f(2) * f(3) = 6
- * f(6) = f(2) * f(4) , f(3) * f(3) = 9
+ * f(4) = f(2) * f(2)
+ * f(5) = f(2) * f(3)
  * https://leetcode-cn.com/problems/jian-sheng-zi-lcof/
  */
 /**
  * @param {number} n
  * @return {number}
  */
-var cuttingRope = function(n) {
+ var cuttingRope = function(n) {
     if(n === 2) {
         return 1
     } else if(n === 3) {
@@ -21,10 +20,12 @@ var cuttingRope = function(n) {
     dp[2] = 2
     dp[3] = 3
 
-    for(let i=4;i<n;i++) {
-        for(let j=2;j<n/2;j++) {
+    for(let i=4;i<=n;i++) {
+        for(let j=2;j<=i/2;j++) {
             dp[i] = Math.max(dp[i], dp[j] * dp[i-j])
         }
     }
     return dp[n]
 };
+
+cuttingRope(10)
