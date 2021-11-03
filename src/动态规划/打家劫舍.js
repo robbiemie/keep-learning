97762bb6ddac8a2f4d59dfe3cfa76b62ce0c5e36
robbiemie <yang657850144@163.com>
2021-11-03 15:38:37 +0800

    feat: 打家劫舍

--- src/动态规划/打家劫舍.js
+++ src/动态规划/打家劫舍.js
@@ -4,30 +4,21 @@
  * 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
  * https://leetcode-cn.com/problems/house-robber
  * 思路:
- * 1. f(k): 前k个房间所能获取的总财产
- * 2. a(k): 第k个房间的财产
- * 3. f(k) = max(f(k-2) + a(k), f(k-1))
+ * f(k) = Math.max(f(k-2) + ak, f(k-1))
  */
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
+
  var rob = function(nums) {
-    if(nums.length < 1) return 0
-   let max = nums[0]
-   if(nums.length === 1) return max
-   if(nums.length === 2) {
-       max = Math.max(nums[0], nums[1])
-       return max
-   }
-   let fk = [0, max]
-   for(let i = 2; i <= nums.length; i++) {
-       let value = Math.max(fk[i-2] + nums[i - 1], fk[i-1])
-       fk.push(value)
-       if(value > max) {
-           max = value
-       } 
-   }
-   return max
+    if(nums.length <= 2) return Math.max(...nums)
+    let dp = new Array(nums.length).fill(0)
+    dp[0] = nums[0]
+    dp[1] = Math.max(...nums.slice(0,2))
+    for(let i=2;i<nums.length;i++) {
+        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1])
+    }
+    return dp[nums.length-1]
 };
