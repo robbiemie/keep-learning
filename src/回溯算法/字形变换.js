4c140c3ef8f73ed05dacc11cbcf95fdd113bfe4a
robbiemie <yang657850144@163.com>
2021-11-24 18:50:48 +0800

    feat: 字形变换

---
+++ src/回溯算法/字形变换.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=6 lang=javascript
+ *
+ * [6] Z 字形变换
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+
+};
+// @lc code=end
+
+
