78a9bb9bb7491ad1d56b165914cda1ea6ed04fe0
robbiemie <yang657850144@163.com>
2021-12-10 10:00:09 +0800

    feat: 搜索旋转排序数组

---
+++ src/数组/搜索旋转排序数组.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=javascript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    return nums.indexOf(target)
+};
+// @lc code=end
+
