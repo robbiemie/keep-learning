39cc464f7917676896aeaaf48a3af706fa3c45bf
robbiemie <yang657850144@163.com>
2022-01-14 09:32:16 +0800

    feat: 全排列-ii

---
+++ src/回溯算法/全排列-ii.js
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=47 lang=javascript
+ *
+ * [47] 全排列 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+  let result = []
+  let map = {}
+  nums.sort()
+
+  function fn(arr1, arr2) {
+    
+    if(arr1.length === nums.length) {
+      const key = arr1.join('')
+      if(!map[key]) {
+        map[key] = 1
+        result.push(arr1)
+      }
+      return
+    }
+
+    for(let i=0;i<arr2.length;i++) {
+      let temp = arr2.slice()
+      temp.splice(i,1)
+      fn(arr1.concat(arr2[i]),temp)
+    }
+
+  }
+  fn([], nums.concat([]))
+
+  return result
+};
+// @lc code=end
+
+
+permuteUnique([3,1,1,2])
