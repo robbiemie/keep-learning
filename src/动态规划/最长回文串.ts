ccc8f59032bbbb66b8b38eeacaef94b912b7aceb
You <xin.yang02@mihoyo.com>
2024-01-29 16:23:43 +0800

    feat: 最长回文串

--- src/动态规划/最长回文串.ts
+++ src/动态规划/最长回文串.ts
@@ -28,3 +28,36 @@ function longestPalindrome(s: string): string {
   }
   return reverse
 };
+
+// Manacher`s 算法
+function longestPalindrome1(s: string): string {
+  if (s.length === 1) return s;
+  let start = 0;
+  let end = 0;
+  let maxLen = 0
+
+  function expandAroundCenter(left: number, right: number): number {
+      while(left >= 0 && right < s.length && s[left] === s[right]) {
+          left--;
+          right++;
+      }
+      return right - left - 1;
+  }
+
+
+  for(let i = 0; i < s.length; i++) {
+      // 查找 aba 类型
+      let length1 = expandAroundCenter(i, i);
+      // 查找 abba 类型
+      let length2 = expandAroundCenter(i, i+1);
+      let len = Math.max(length1, length2);
+
+      if(len > maxLen) {
+          maxLen = len
+          let mod = length1 > length2 ?  0 : 1;
+          start = i - Math.floor(len / 2) + mod; // 可优化
+          end = i + Math.floor(len / 2) + 1;
+      }
+  }
+  return s.slice(start, end)
+}
