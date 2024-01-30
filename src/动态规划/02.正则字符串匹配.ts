d791fb8db1b6ea1417d42cd5862b467711e0bd42
You <xin.yang02@mihoyo.com>
2024-01-30 16:54:12 +0800

    feat: 正则字符串匹配

--- src/动态规划/02.正则字符串匹配.ts
+++ src/动态规划/02.正则字符串匹配.ts
@@ -1,5 +1,5 @@
 function isMatch(s: string, p: string): boolean {
-  let dp: boolean[][] = [];
+  let dp = [];
   // 初始化动态规划数组
   for (let i = 0; i <= s.length; i++) {
       dp[i] = new Array(p.length + 1).fill(false);
@@ -11,10 +11,11 @@ function isMatch(s: string, p: string): boolean {
   // 处理 p[j] 中可能存在 '*' 的情况
   for (let j = 1; j <= p.length; j++) {
       if (p[j - 1] === '*') {
+          // s为空字符
           dp[0][j] = dp[0][j - 2];
       }
   }
-  console.log(dp)
+  // console.log(dp)
   for(let i=1;i<=s.length;i++) {
       for(let j=1;j<=p.length;j++) {
           if(s[i-1] === p[j-1]) {
@@ -25,11 +26,10 @@ function isMatch(s: string, p: string): boolean {
               dp[i][j] = dp[i-1][j-1]
           } else if(p[j-1] === '*') {
               // '*'字符匹配
-              // j-1:为当前元素, j-2:即前一个元素
               dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s[i-1] === p[j-2] || p[j-2] === '.'))
           }
       }
   }
-  console.log('output:', dp)
+  // console.log('output:', dp)
   return dp[s.length][p.length];
 };
