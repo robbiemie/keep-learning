4b9eb0ae1e789d5bae2bd01d7066466ee0634336
You <xin.yang02@mihoyo.com>
2024-01-31 17:20:04 +0800

    feat: initial demo

---
+++ demo/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <section class="demo1">
+      <!-- 清除元素浮动 -->
+      <div class="parent">
+        <div class="child">1</div>
+        <div class="child">2</div>
+        <div class="child">3</div>
+        <div class="child">4</div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+.demo1 {
+  .parent{
+      width: 350px;
+      background-color: blue;
+      margin: 10px auto;
+      display: block;
+      /* overflow: hidden; */
+  }
+  .child{
+      width: 100px;
+      height: 100px;
+      float: left;
+      line-height: 100px;
+      text-align: center;
+      background-color: cornsilk;
+  }
+}
+</style>
