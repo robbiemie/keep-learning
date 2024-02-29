a7d13f22de663f64816286b1d9069cd0a06d77c5
Robbie <yang657850144@163.com>
2024-02-29 15:01:49 +0800

    Update 3.js

--- leetcode/编程题/快手/3.js
+++ leetcode/编程题/快手/3.js
@@ -16,3 +16,42 @@ process.nextTick(() => {
 setImmediate(() => {
     console.log(5) // 宏2
 })
+
+
+///////////变中题型
+setTimeout(() => {
+  console.log('1') 
+}, 0)
+
+new Promise((resolve, reject) => {
+  console.log('2')
+  debugger
+  process.nextTick(resolve)
+}).then(_ => {
+  console.log('3') 
+})
+
+process.nextTick(() => {
+  console.log('4')
+})
+
+new Promise((resolve, reject) => {
+  process.nextTick(resolve)
+}).then(_ => {
+  console.log('5') 
+}).then(_ => {
+  console.log('6') 
+})
+setImmediate(() => {
+  console.log(7) 
+})
+
+// 2,4,3,5,6,1,7
+/**
+ *
+- 首先执行同步代码 2
+- 然后进入微任务队列：4
+- 进入下一轮微任务队列：3,5
+- 进入下一轮微任务队列：6
+- 执行宏任务：1,7
+ */
