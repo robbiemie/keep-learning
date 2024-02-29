1b03fda8e550d48342833f1930f8e820b89073a6
Robbie <yang657850144@163.com>
2024-02-29 15:20:47 +0800

    Update 5.js

--- leetcode/编程题/快手/5.js
+++ leetcode/编程题/快手/5.js
@@ -15,3 +15,36 @@ newArr = [
     {a:'1', b:2}
 ]
  */
+
+
+let arr = [
+  {a: 1, b: 2},
+  {b: 2, a: 1},
+  {a:'1', b:2}
+]
+
+function filters(arr) {
+  const list = arr.slice()
+  let set = new Set()
+  for(let item of list) {
+    set.add(JSON.stringify({
+      a: item['a'],
+      b: item['b']
+    }))
+  }
+
+  return [...set].map(item => JSON.parse(item));
+}
+
+/**
+* 去重
+* 要求: 时间复杂度尽量最低
+*/
+const newArr = filters(arr)
+console.log(newArr)
+/**
+newArr = [
+  {a: 1, b: 2},
+  {a:'1', b:2}
+]
+*/
