4ce576e57415ac13abbc89fbe60062f5714a33ad
You <xin.yang02@mihoyo.com>
2024-03-02 20:05:19 +0800

    feat: 大数相乘

--- docs/leetcode/字符串/09.大数相乘.ts
+++ docs/leetcode/字符串/09.大数相乘.ts
@@ -1,28 +1,24 @@
 function multiply(num1: string, num2: string): string {
 
-  function reverse(str: string = ''):number[] {
-      return str.split('').reverse().map(item => +item);
-  }
+    function reverse(str: string = ''):number[] {
+        return str.split('').reverse().map(item => +item);
+    }
 
-  let arrReverseA = reverse(num1);
-  let arrReverseB = reverse(num2);
-  let total = new Array(arrReverseA.length + arrReverseB.length).fill(0);
+    let arrReverseA = reverse(num1);
+    let arrReverseB = reverse(num2);
+    let total = new Array(arrReverseA.length + arrReverseB.length).fill(0);
 
-  //console.log(`arrReverseA: ${arrReverseA}, arrReverseB:${arrReverseB}`);
-  let index = 0
-  for(let i=0;i<arrReverseA.length;i++) {
-      let left = arrReverseA[i]
-      index = i;
-      for(let j=0;j<arrReverseB.length;j++) {
-          let result = left * arrReverseB[j] + total[index]
-          total[index] = result % 10
-          total[index + 1] += Math.floor(result / 10);
-          index++;
-      }
-  }
-  // while(total[total.length - 1] === 0) {
-  //  total.pop()   
-  // }
-  let str = total.reverse().join('').replace(/^0+(?=\d+)/, '');
-  return str
+    //console.log(`arrReverseA: ${arrReverseA}, arrReverseB:${arrReverseB}`);
+    let index = 0
+    for(let i=0;i<arrReverseA.length;i++) {
+        let left = arrReverseA[i]
+        index = i;
+        for(let j=0;j<arrReverseB.length;j++) {
+            let result = left * arrReverseB[j] + total[index]
+            total[index] = result % 10
+            total[index + 1] += Math.floor(result / 10);
+            index++;
+        }
+    }
+    return total.reverse().join('').replace(/^0+(?=\d+)/, '');
 };
