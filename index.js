5b8598b77fa8c8c5b6d41b17c8988b31261890fe
robbiemie <yang657850144@163.com>
2024-03-08 01:06:38 +0800

    feat: 性能指标

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-// 给出如下 json
-const json = {
-  a: {
-    b: {
-      c: 1
-    }
-  },
-  d: {
-    e: 2
-  }
-}
-// 按照如下路径索引
-const arr = ['a', 'b', 'c']
-
-function findPath(json, arr) {
-  let obj = json
-  for(let key of arr) {
-    obj = obj[key]
-  }
-  return obj;
-}
-
-
-const res = findPath(json, arr)
-console.log(res)
+new Promise(resolve => {
+  setTimeout(() => {
+    resolve('3');
+    console.log('t1')
+  })
+  resolve('1')
+  resolve('2')
+}).then(res => {
+  console.log(res);
+  return res
+}).then(res=> {
+  console.log(res)
+}).finally(res => {
+  console.log(res)
+})
