fdd6b4fe1603802e3eb164ef618555a2d126f131
You <xin.yang02@mihoyo.com>
2024-03-09 18:19:30 +0800

    feat: update

--- docs/leetcode/编程题/快手/2.js
+++
@@ -1,9 +0,0 @@
-var a = 1
-
-function fn() {
-    console.log(a)
-    var a = 2 // undefined
-    // 如果是这个： let a = 2 throw error
-}
-
-fn()
--- docs/leetcode/编程题/蓝湖/五子棋/qizi.js
+++
@@ -1,7 +0,0 @@
-export default class Qizi {
-    constructor(props) {
-        this.type = props.type
-        this.x = 0
-        this.y = 0
-    }
-}
